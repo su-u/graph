c616e3ced7ac5d1a06f78a4a87e64100d68a813e
すー <aaaiiiuuu35@gmail.com>
2019-06-06 17:00:21 +0900

    Update Graph.py

--- Graph/Graph/Graph.py
+++ Graph/Graph/Graph.py
@@ -26,17 +26,21 @@ l = data.get_array_of_samples()[-1000:]
 
 
 # 簡単な信号の作成
-N = 128 # サンプル数
+N = 180 # サンプル数
 dt = 0.01 # サンプリング周期(sec):100ms =>サンプリング周波数100Hz
 freq = 5 # 周波数(10Hz) =>正弦波の周期0.1sec
 amp = 1 # 振幅
 t = np.arange(0, N*dt, dt) # 時間軸
 f = amp * np.sin(2*np.pi*freq*t) # 信号（周波数10、振幅1の正弦波）
 
-#f1 = 2 # 周期
-#f2 = 6 
-#n = np.arange(N)
-#f = np.sin(f1 * 2 * np.pi * (n/N)) + np.sin(f2 * 2 * np.pi * (n/N)) 
+N = 100 # データ数
+n = np.arange(N)
+f1 = 5 # 周期①
+f2 = 10 # 周期②
+
+a1 = 1 # 振幅①
+a2 = 5 # 振幅②
+f = a1 * np.sin(f1 * 2 * np.pi * (n/N)) + a2 * np.sin(f2 * 2 * np.pi * (n/N)) 
 
 ## 高速フーリエ変換(FFT)
 F = np.fft.fft(f) #
@@ -53,16 +57,17 @@ fq = np.linspace(0, 1.0/dt, N) # 周波数軸　linspace(開始,終了,分割数
 # グラフ表示
 fig = plt.figure(figsize=(12, 7))
 # 信号のグラフ（時間軸）
-plt.title("時間領域", **igfont)
+#plt.title("時間領域", **igfont)
 ax2 = fig.add_subplot(211)
 plt.xlabel('time(sec)', fontsize=14)
 plt.ylabel('amplitude', fontsize=14)
-plt.plot(t, f)
+plt.plot(f)
 
 # FFTのグラフ（周波数軸）
 ax2 = fig.add_subplot(212)
-plt.title("周波数領域", **igfont)
+#plt.title("周波数領域", **igfont)
 plt.xlabel('freqency(Hz)', fontsize=14)
 plt.ylabel('amplitude', fontsize=14)
 plt.plot(fq[:int(N/2)+1], F_abs_amp[:int(N/2)+1]) # ナイキスト定数まで表示
+
 plt.show()
